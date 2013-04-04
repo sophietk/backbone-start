c0abec2ece3f90b3aacd2f135ad58994e899ac77
sophietk <sophie.tk@gmail.com>
2013-04-04 09:49:24 +0200

    Instruction for new collection and /people resources

--- server/server.js
+++ server/server.js
@@ -8,3 +8,12 @@ var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Listening on " + port);
 });
+
+app.get('/people', function (req, res) {
+    res.send([
+        {_id: 1, name: "Jack", avatar: "/img/skellington.png"},
+        {_id: 2, name: "Frank", avatar: "/img/frankenstein.png"},
+        {_id: 3, name: "Vlad", avatar: "/img/dracula.png"},
+        {_id: 4, name: "Paul", avatar: "/img/alien.png"}
+    ]);
+});
--- webapp/js/src/HomeView.js
+++ webapp/js/src/HomeView.js
@@ -5,10 +5,13 @@ var HomeView = Backbone.View.extend({
     render: function () {
         this.$el.html('<div class="page-header"><h1>HomeView</h1></div>'
             + '<p class="lead">Welcome on the home view.</p>'
-            + '<p>The router displays this view when you call <a href="#home">#home route</a>, and it is also the <a href="#whatever">default route</a>.</p>'
+            + '<p>The <span class="router">router</span> displays this view when you call <a href="#home">#home</a> route, and it is also the <a href="#whatever">default route</a>.</p>'
             + '<p>Here is the application we want to build :</p>'
             + '<img src="img/diagram.png" alt="Backbone app diagram">'
-            + '<ol><li>Try to make your own view (PeopleView) connected to <a href="#people">#people route</a>.</li></ol>'
+            + '<ol>'
+            + '<li>Try to make your own <span class="view">view</span> (PeopleView) connected to <a href="#people">#people</a> route.</li>'
+            + '<li>In this view, call a new <span class="collection">collection</span> (PeopleCollection) that fetches the <a href="/people">/people</a> url resources.</li>'
+            + '</ol>'
         );
 
         return this;
