7965819c6c741facb5123f67ab9ca583dece6d5c
sophietk <sophie.tk@gmail.com>
2013-04-04 09:36:35 +0200

    App diagram

--- webapp/js/src/HomeView.js
+++ webapp/js/src/HomeView.js
@@ -6,7 +6,9 @@ var HomeView = Backbone.View.extend({
         this.$el.html('<div class="page-header"><h1>HomeView</h1></div>'
             + '<p class="lead">Welcome on the home view.</p>'
             + '<p>The router displays this view when you call <a href="#home">#home route</a>, and it is also the <a href="#whatever">default route</a>.</p>'
-            + '<p>Try to make your <a href="#people">own view</a> connected to #people route.</p>'
+            + '<p>Here is the application we want to build :</p>'
+            + '<img src="img/diagram.png" alt="Backbone app diagram">'
+            + '<ol><li>Try to make your <a href="#people">own view</a> connected to #people route.</li></ol>'
         );
 
         return this;
