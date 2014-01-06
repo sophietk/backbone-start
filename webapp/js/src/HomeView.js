4554bf34c52db3485554d92a86209051ce7850e9
sophietk <sophie.tk@gmail.com>
2014-01-06 21:01:19 +0100

    Update css libraries

--- webapp/js/src/HomeView.js
+++ webapp/js/src/HomeView.js
@@ -3,16 +3,8 @@ var HomeView = Backbone.View.extend({
     id: 'home-view',
 
     render: function () {
-        this.$el.html('<div class="page-header"><h1>HomeView</h1></div>'
-            + '<p class="lead">Welcome on the home view.</p>'
-            + '<p>The <span class="router">router</span> displays this view when you call <a href="#home">#home</a> route, and it is also the <a href="#whatever">default route</a>.</p>'
-            + '<p>Here is the application we want to build :</p>'
-            + '<img src="img/diagram.png" alt="Backbone app diagram">'
-            + '<ol>'
-            + '<li>Try to make your own <span class="view">view</span> (PeopleView) connected to <a href="#people">#people</a> route.</li>'
-            + '<li>In this view, call a new <span class="collection">collection</span> (PeopleCollection) that fetches the <a href="/people">/people</a> url resources.</li>'
-            + '</ol>'
-        );
+        var template = $("#homeview-tutorial-tpl").html();
+        this.$el.html(template);
 
         return this;
     }
